58bfc2140109cb8f095b13fef7668ae544d0693f
yjiro0403 <jiromyworks@gmail.com>
2019-09-29 20:29:34 +0900

    Add files via upload

---
+++ arduinoCentral/arduinoCentral.ino
@@ -0,0 +1,209 @@
+// 仕様：ROW側がカソード、COLOMN側がアノードの8x8マトリックスLED
+const int RPIN[8]   = { 6, 7, 8, 9, 10, 11, 12, 13 };
+const int CATHODEPIN[8] = {  2,  3,  4,  5,  14,  15,  16,  17 };
+
+const int sx = 8;
+const int sy = 8;
+const float density = 0.5; 
+int Rworld[8][8][2];
+int Gworld[8][8][2];
+int Bworld[8][8][2];
+
+const int b_size = 1;
+int counter = 0;
+
+void setup()
+{
+  // アノードを、LOWで初期化
+  for( int i = 0; i < 8; i++ )
+  {
+    pinMode( RPIN[i], OUTPUT );
+    digitalWrite( RPIN[i], LOW );
+  }
+
+  for( int i = 0; i < 8; i++ )
+  {
+    pinMode( CATHODEPIN[i], OUTPUT );
+    digitalWrite( CATHODEPIN[i], HIGH );
+  }
+
+  //Make Stage
+  randomSeed(analogRead(5));
+  for (int i = 0; i < sx * sy * density; i++) { 
+    Rworld[(int)random(sx)][(int)random(sy)][1] = 1; 
+    Gworld[(int)random(sx)][(int)random(sy)][1] = 1; 
+    Bworld[(int)random(sx)][(int)random(sy)][1] = 1; 
+  }
+  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+
+  
+    //DRAW STAGE
+    //DRAW RWORLD
+    
+    for (int y = 0; y < sy; y++) {
+     if (CATHODEPIN[y] < 6) { PORTD &= ~_BV(CATHODEPIN[y]);}
+     else { PORTC &= ~_BV(CATHODEPIN[y]-14); }
+
+      for (int x = 0; x < sx; x++) {       
+          if ((Rworld[x][y][1] == 1) || (Rworld[x][y][1] == 0 && Rworld[x][y][0] == 1)) {
+            Rworld[x][y][0] = 1;
+
+            //OUTPUT
+            if (RPIN[x] < 8) {
+              PORTD |= _BV(RPIN[x]);
+              } else {
+                  PORTB |= _BV(RPIN[x]-8);
+                }            
+          }
+             
+          if (Rworld[x][y][1] == -1) {Rworld[x][y][0] = 0;}
+          Rworld[x][y][1] = 0;
+          
+        }
+        
+        delayMicroseconds(100);
+ 
+        //RESET
+        PORTD &= ~(_BV(6) | _BV(7));
+        PORTB &= ~(_BV(0) | _BV(1) | _BV(2) | _BV(3) | _BV(4) | _BV(5));
+        if (CATHODEPIN[y] < 6) { PORTD |= _BV(CATHODEPIN[y]);}
+        else { PORTC |= _BV(CATHODEPIN[y]-14); }
+        }
+
+
+
+    ///
+    // DRAW BWORLD
+    ///
+    for (int y = 0; y < sy; y++) {
+     if (CATHODEPIN[y] < 6) { PORTD &= ~_BV(CATHODEPIN[y]);}
+     else { PORTC &= ~_BV(CATHODEPIN[y]-14); }
+          
+     Serial.print('B');
+          
+      for (int x = 0; x < sx; x++) {
+          if ((Bworld[x][y][1] == 1) || (Bworld[x][y][1] == 0 && Bworld[x][y][0] == 1)) {
+            Bworld[x][y][0] = 1;
+              
+            Serial.print(1);
+            } else {
+            Serial.print(0);            
+              }
+              
+          if (Bworld[x][y][1] == -1) {Bworld[x][y][0] = 0;}
+          Bworld[x][y][1] = 0; 
+        }
+        
+        while (!endCheck());
+
+      //RESET
+      if (CATHODEPIN[y] < 6) { PORTD |= _BV(CATHODEPIN[y]);}
+      else { PORTC |= _BV(CATHODEPIN[y]-14); }
+      }
+
+
+    ///
+    // DRAW GWORLD
+    ///
+    for (int y = 0; y < sy; y++) {
+     if (CATHODEPIN[y] < 6) { PORTD &= ~_BV(CATHODEPIN[y]);}
+     else { PORTC &= ~_BV(CATHODEPIN[y]-14); }
+          
+     Serial.print('G');
+          
+      for (int x = 0; x < sx; x++) {
+          if ((Gworld[x][y][1] == 1) || (Gworld[x][y][1] == 0 && Gworld[x][y][0] == 1)) {
+            Gworld[x][y][0] = 1;
+              
+            Serial.print(1);
+            } else {
+            Serial.print(0);            
+              }
+              
+          if (Gworld[x][y][1] == -1) {Gworld[x][y][0] = 0;}
+          Gworld[x][y][1] = 0; 
+        }
+        
+        while (!endCheck());
+
+      //RESET
+      if (CATHODEPIN[y] < 6) { PORTD |= _BV(CATHODEPIN[y]);}
+      else { PORTC |= _BV(CATHODEPIN[y]-14); }
+      }
+
+  //Change STAGE
+  counter++;
+  int count;
+  if((counter%10) == 0) {
+   for (int x = 0; x < sx; x++) { 
+    for (int y = 0; y < sy; y++) { 
+      count = Rneighbors(x, y); 
+      //Serial.println(count);
+      if (count == 3 && Rworld[x][y][0] == 0) { Rworld[x][y][1] = 1; } 
+      if ((count < 2 || count > 3) && Rworld[x][y][0] == 1) { Rworld[x][y][1] = -1; }
+
+      count = Gneighbors(x, y); 
+      //Serial.println(count);
+      if (count == 3 && Gworld[x][y][0] == 0) { Gworld[x][y][1] = 1; } 
+      if ((count < 2 || count > 3) && Gworld[x][y][0] == 1) { Gworld[x][y][1] = -1; }
+
+      count = Bneighbors(x, y); 
+      //Serial.println(count);
+      if (count == 3 && Bworld[x][y][0] == 0) { Bworld[x][y][1] = 1; } 
+      if ((count < 2 || count > 3) && Bworld[x][y][0] == 1) { Bworld[x][y][1] = -1; }
+    }
+   }
+  }
+}
+
+//足りない
+inline int Rneighbors(int x, int y) 
+{ 
+  return Rworld[(x + 1) % sx][y][0] + 
+         Rworld[x][(y + b_size) % sy][0] + 
+         Rworld[(x + sx - b_size) % sx][y][0] + 
+         Rworld[x][(y + sx - b_size) % sx][0] + 
+         Rworld[(x + b_size) % sx][(y + b_size) % sy][0] + 
+         Rworld[(x + sx - b_size) % sx][(y + b_size) % sy][0] + 
+         Rworld[(x + sx - b_size) % sx][(y + sy - b_size) % sy][0] + 
+         Rworld[(x + b_size) % sx][(y + sy - b_size) % sy][0]; 
+}
+
+inline int Gneighbors(int x, int y) 
+{ 
+  return Gworld[(x + 1) % sx][y][0] + 
+         Gworld[x][(y + b_size) % sy][0] + 
+         Gworld[(x + sx - b_size) % sx][y][0] + 
+         Gworld[x][(y + sx - b_size) % sx][0] + 
+         Gworld[(x + b_size) % sx][(y + b_size) % sy][0] + 
+         Gworld[(x + sx - b_size) % sx][(y + b_size) % sy][0] + 
+         Gworld[(x + sx - b_size) % sx][(y + sy - b_size) % sy][0] + 
+         Gworld[(x + b_size) % sx][(y + sy - b_size) % sy][0]; 
+}
+
+inline int Bneighbors(int x, int y) 
+{ 
+  return Bworld[(x + 1) % sx][y][0] + 
+         Bworld[x][(y + b_size) % sy][0] + 
+         Bworld[(x + sx - b_size) % sx][y][0] + 
+         Bworld[x][(y + sx - b_size) % sx][0] + 
+         Bworld[(x + b_size) % sx][(y + b_size) % sy][0] + 
+         Bworld[(x + sx - b_size) % sx][(y + b_size) % sy][0] + 
+         Bworld[(x + sx - b_size) % sx][(y + sy - b_size) % sy][0] + 
+         Bworld[(x + b_size) % sx][(y + sy - b_size) % sy][0]; 
+}
+
+inline boolean endCheck() {
+  if(Serial.available()){
+      char var = Serial.read();
+      if (var == 'E') {return true;}
+    }
+
+    return false;
+}
---
+++ arduinoGB/arduinoGB.ino
@@ -0,0 +1,62 @@
+int BPIN[8]   = { 2, 3, 4, 5, 6, 7, 8, 9 };
+int GPIN[8] = {  10,  11,  12,  13,  14,  15,  16,  17 };
+
+void setup()
+{
+  // アノードを、LOWで初期化
+  for( int i = 0; i < 8; i++ )
+  {
+    pinMode( BPIN[i], OUTPUT );
+    digitalWrite( BPIN[i], LOW );
+  }
+
+  // カソードを、HIGHで初期化
+  for( int i = 0; i < 8; i++ )
+  {
+    pinMode( GPIN[i], OUTPUT );
+    digitalWrite( GPIN[i], LOW );
+  }
+  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+    // 受信バッファに３バイト（ヘッダ＋int）以上のデータが着ているか確認
+  while ( !Serial.available());
+    char checkBit = Serial.read();
+
+    for (int x = 0; x < 8; x++) {  
+        while ( !Serial.available() );
+        char val = Serial.read();
+
+        if (checkBit == 'G') {
+         if (val == '1') {
+          if(GPIN[x] < 14) { PORTB |= _BV(GPIN[x]-8);}
+          else {PORTC |= _BV(GPIN[x]-14);}
+          }
+        } else if (checkBit == 'B') {
+         if (val == '1') {
+          if(BPIN[x] < 8) { PORTD |= _BV(BPIN[x]);}
+          else {PORTB |= _BV(BPIN[x]-8);}
+          }
+        }
+      }
+
+      delayMicroseconds(0);
+
+      if (checkBit == 'G') {
+        //for (int x = 0; x < 8; x++) {digitalWrite(GPIN[x], LOW);}
+        PORTB &= ~(_BV(2) | _BV(3) | _BV(4) | _BV(5));
+        PORTC &= ~(_BV(0) | _BV(1) | _BV(2) | _BV(3));
+      } else if (checkBit == 'B') {
+        //for (int x = 0; x < 8; x++) {digitalWrite(BPIN[x], LOW);}
+        PORTD &= ~(_BV(2) | _BV(3) | _BV(4) | _BV(5) | _BV(6) | _BV(7));
+        PORTB &= ~(_BV(0) | _BV(1));
+        }
+      delayMicroseconds(5000);
+      Serial.print('E'); //End this row
+
+      
+
+}
